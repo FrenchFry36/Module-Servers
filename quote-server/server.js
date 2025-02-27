8210e6ac5e27046756e87b23444786ab134eac5d
FrenchFry36 <saidakbarov@protonmail.com>
2025-02-27 17:06:37 +0100

    level 1 is done

--- quote-server/server.js
+++ quote-server/server.js
@@ -4,9 +4,13 @@
 //load the 'express' module which makes writing webservers easy
 import express from "express";
 //load the quotes JSON
-import quotes from "./quotes.json" assert { type: "json" };
+import fs from "fs";
+
+// Read and parse the JSON file
+const quotes = JSON.parse(fs.readFileSync("./quotes.json", "utf-8"));
 
 const app = express();
+const port = 3001;
 // Now register handlers for some routes:
 //   /                  - Return some helpful welcome info (text)
 //   /quotes            - Should return all quotes (json)
@@ -16,7 +20,13 @@ app.get("/", (request, response) => {
 });
 
 //START OF YOUR CODE...
+app.get("/quotes", (request, response) => {
+  response.json(quotes);
+});
 
+app.get("/quotes/random", (request, response) => {
+  response.json(pickFromArray(quotes));
+});
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
@@ -27,6 +37,8 @@ const pickFromArray = (arrayofQuotes) =>
   arrayofQuotes[Math.floor(Math.random() * arrayofQuotes.length)];
 
 //Start our server so that it listens for HTTP requests!
-const listener = app.listen(3001, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+const listener = app.listen(port, () => {
+  console.log(
+    "Your app is listening on port http://localhost:" + listener.address().port
+  );
 });
